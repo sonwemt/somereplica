68fa435281ffe5edb8a80bc6fc64511cd12efab3
Sondre <sonfjell@gmail.com>
2022-11-09 00:47:06 +0100

    Add more css to overview

--- src/components/page/PostOverview.js
+++ src/components/page/PostOverview.js
@@ -5,21 +5,25 @@ import { Votes } from "./Votes";
 function PostOverview({posts, upvote, downvote}) {
   return (
   <div id="PostContainer">
-    <Link to="/submitpost" ><button>submit</button></Link>
-    <ul className="postOverview">
+    <Link to="/submitpost" className="submit-link">
+      Submit
+    </Link>
+    <ul className="post-list">
       {posts.map((post) => {
         return (
-        <li key={post.id}>
+        <li className="post-item" key={post.id}>
           <Votes post={post} upvote={upvote} downvote={downvote} />
-          {
-          post.linkExternal ? <a href={`${post.content}`}>{post.title}</a> :
-          <Link to={`/${post.id}`} state={{id: post.id}} >
-            <div>{post.title}</div>
-          </Link>
-          }
-          <Link to={`/${post.id}`} state={{id: post.id}} >
-            <div className="comment-link">comments</div>
-          </Link>
+          <div className="post-item-content">
+            {
+            post.linkExternal ? <a href={`${post.content}`}>{post.title}</a> :
+            <Link to={`/${post.id}`} state={{id: post.id}} >
+              <div>{post.title}</div>
+            </Link>
+            }
+            <Link to={`/${post.id}`} state={{id: post.id}} >
+              <div className="comment-link">comments</div>
+            </Link>
+          </div>
           </li>
         )
       })}
