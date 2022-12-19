138ba930fab0a8885dac4a7d79a4a257f7e519f6
Sondre <sonfjell@gmail.com>
2022-12-19 18:13:14 +0100

    Fix and simplify TimeElapsed

--- src/components/page/TimeElapsed.js
+++ src/components/page/TimeElapsed.js
@@ -1,29 +1,38 @@
 import { useEffect, useState } from "react";
 
 function TimeElapsed({ created }) {
-  const [timeValue, setTimeValue] = useState(null);
-  const [timeType, setTimeType] = useState(null);
+  const [timeElapsed, setTimeElapsed] = useState(null);
+  
+  // taken from: https://stackoverflow.com/a/69122877
+  const timeAgo = (input) => {
+    const date = (input instanceof Date) ? input : new Date(input);
+    const formatter = new Intl.RelativeTimeFormat('en');
+    const ranges = {
+      years: 3600 * 24 * 365,
+      months: 3600 * 24 * 30,
+      weeks: 3600 * 24 * 7,
+      days: 3600 * 24,
+      hours: 3600,
+      minutes: 60,
+      seconds: 1
+    };
+    const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+    for (let key in ranges) {
+      if (ranges[key] < Math.abs(secondsElapsed)) {
+        const delta = secondsElapsed / ranges[key];
+        return formatter.format(Math.round(delta), key);
+      }
+    }
+  }
 
   useEffect(() => {
-    const clientDate = new Date();
     const creationDate = created.toDate();
-    if(-(clientDate.getDay() - creationDate.getDay()) < 1) {
-      if(-(clientDate.getHours() - creationDate.getHours()) < 1) {
-        setTimeValue((clientDate.getMinutes() - creationDate.getMinutes()));
-        setTimeType('minutes');
-      } else {
-        setTimeValue((clientDate.getHours() - creationDate.getHours()));
-        setTimeType('hours');
-      }
-    } else {
-      setTimeValue(-(clientDate.getDay() - creationDate.getDay()));
-      setTimeType('days');
-    }
+    setTimeElapsed(timeAgo(creationDate));
   }, [created])
 
-  return <div className="time-container">
-    {timeValue !== null && timeType !== null ? <div style={{color: 'gray'}}>{`${timeValue} ${timeType} ago`}</div>: null}
-  </div>
+  return (<div className="time-container">
+    {timeElapsed !== null ? <div style={{color: 'gray'}}>{ timeElapsed }</div>:null}
+  </div>);
 }
 
 export { TimeElapsed };
